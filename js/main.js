ba7773d832f7c38879fc5586a3c101fa5a927b11
alienat3d <72837855+alienat3d@users.noreply.github.com>
2021-04-19 13:10:46 +0300

    19.04 adaptive-2

--- js/main.js
+++ js/main.js
@@ -4,6 +4,22 @@ $(function(){
     arrows: false,
     dots: true,
     slidesToShow: 3,
+    responsive: [
+      {
+        breakpoint: 841,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        }
+      },
+      {
+        breakpoint: 601,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        }
+      }
+    ],
   });
 
 });
